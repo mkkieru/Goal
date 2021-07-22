944e09c10f38edc5fb8c74b7c476101b3593570e
mkkieru <mkieru55@gmail.com>
2021-07-22 12:37:21 +0300

    Add Goals app

---
+++ src/app/goal-details/goal-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit , EventEmitter , Input, Output} from '@angular/core';
+//import { EventEmitter } from 'protractor';
+import { Goal } from "../goal";
+
+@Component({
+  selector: 'app-goal-details',
+  templateUrl: './goal-details.component.html',
+  styleUrls: ['./goal-details.component.css']
+})
+export class GoalDetailsComponent implements OnInit {
+
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/strikethriugh.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StrikethriughDirective } from './strikethriugh.directive';
+
+describe('StrikethriughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrikethriughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
